7da465221dc02067bc4220ed2a88711164dce8f4
Ever <everjavier95@hotmail.com>
2019-02-15 15:14:56 -0800

    Adding device start files

--- installApp.sh
+++ installApp.sh
@@ -1,5 +1,5 @@
-cd home/enroute/Android/Sdk/platform-tools/
+cd /home/enroute/Android/Sdk/platform-tools
 echo "Installing App [Pixel 2 XL] ..."
-./adb install home/enroute/Documents/PreciseUnitConversion.apk
+./adb install /home/enroute/Documents/PreciseUnitConversion.apk
 echo "Emulator Closed [Pixel 2 XL]"
 
